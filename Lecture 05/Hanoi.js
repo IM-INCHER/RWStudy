f6b42b0c6b8fbbc1fa41561fd165e279beb1ec6d
IM-IMCHER <ssrty12150@naver.com>
2022-04-13 16:50:16 +0900

    [IC] 7주차 과제

--- Lecture 05/Hanoi.js
+++ Lecture 05/Hanoi.js
@@ -1,24 +1,140 @@
 var btn = document.getElementById("reBtn");
 var first = document.getElementById("first");
+var second = document.getElementById("second");
+var third = document.getElementById("third");
+
+var firstBox = document.getElementById("redBox");
+var secondBox = document.getElementById("greenBox");
+var thirdBox = document.getElementById("blueBox");
+
+var firstArr = [];
+var secondArr = [];
+var thirdArr = [];
+
+var selectArr;
 
 function Init()
 {
-    while(first.firstChild)
-    {
-        first.removeChild(first.firstChild);
-    }
-  
-    first.appendChild(document.createTextNode("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥\n"));
-    first.appendChild(document.createElement('br'));
-    first.appendChild(document.createTextNode("🟨🟨🟨🟨🟨🟨🟨🟨🟨"));
-    first.appendChild(document.createElement('br'));
-    first.appendChild(document.createTextNode("🟩🟩🟩🟩🟩🟩🟩"));
-    first.appendChild(document.createElement('br'));
-    first.appendChild(document.createTextNode("🟦🟦🟦🟦🟦"));
-    first.appendChild(document.createElement('br'));
-    first.appendChild(document.createTextNode("🟪🟪🟪"));
-    first.appendChild(document.createElement('br'));
-    first.appendChild(document.createTextNode("⬛"));
+    firstArr.length = 0;
+    secondArr.length = 0;
+    thirdArr.length = 0;
+    
+    firstArr.push(document.createTextNode("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥"));
+    firstArr.push(document.createTextNode("🟨🟨🟨🟨🟨🟨🟨🟨🟨"));
+    firstArr.push(document.createTextNode("🟩🟩🟩🟩🟩🟩🟩"));
+    firstArr.push(document.createTextNode("🟦🟦🟦🟦🟦"));
+    firstArr.push(document.createTextNode("🟪🟪🟪"));
+    firstArr.push(document.createTextNode("⬛"));
+
+    Draw();
 }
 
-//btn.onclick = Init()
+function Draw()
+{
+    while(first.firstChild)
+        first.removeChild(first.firstChild);
+
+    while(second.firstChild)
+        second.removeChild(second.firstChild);
+
+    while(third.firstChild)
+        third.removeChild(third.firstChild);
+
+    for(var i = 0; i < firstArr.length; i++)
+    {
+        first.appendChild(firstArr[i]);
+        first.appendChild(document.createElement('br'));
+    }
+
+    for(var i = 0; i < secondArr.length; i++)
+    {
+        second.appendChild(secondArr[i]);
+        second.appendChild(document.createElement('br'));
+    }
+
+    for(var i = 0; i < thirdArr.length; i++)
+    {
+        third.appendChild(thirdArr[i]);
+        third.appendChild(document.createElement('br'));
+    }
+}
+
+function ClickRedBox()
+{
+ 
+    if(selectArr != null)
+    {
+        if(selectArr == firstArr)
+        {
+            alert("이미 선택되었습니다")
+        }
+        else
+        {
+            firstArr.push(selectArr.pop());
+            selectArr = null;
+            Draw();
+        }
+    }
+    else
+    {
+        if(firstArr.length > 0)
+        {
+            selectArr = firstArr;
+        }
+        else
+            alert("비어있습니다")
+    }
+            
+}
+
+function ClickGreenBox()
+{
+    if(selectArr != null)
+    {
+        if(selectArr == secondArr)
+        {
+            alert("이미 선택되었습니다")
+        }
+        else
+        {
+            secondArr.push(selectArr.pop());
+            selectArr = null;
+            Draw();
+        }
+    }
+    else
+    {
+        if(secondArr.length > 0)
+        {
+            selectArr = secondArr;
+        }
+        else
+            alert("비어있습니다")
+    }
+}
+
+function ClickBlueBox()
+{
+    if(selectArr != null)
+    {
+        if(selectArr == thirdArr)
+        {
+            alert("이미 선택되었습니다")
+        }
+        else
+        {
+            thirdArr.push(selectArr.pop());
+            selectArr = null;
+            Draw();
+        }
+    }
+    else
+    {
+        if(thirdArr.length > 0)
+        {
+            selectArr = thirdArr;
+        }
+        else
+            alert("비어있습니다")
+    }
+}
